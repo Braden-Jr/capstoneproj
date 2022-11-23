842e0c59b15a7dd34aa269cdff3ff7b69a2fdd1c
Lyca Castro <lccastro@asticom.com.ph>
2022-11-24 01:44:14 +0800

    updated UI

--- resources/views/men.blade.php
+++ resources/views/men.blade.php
@@ -7,7 +7,7 @@
                 <div class="row">
                     <div class="col">
                         <div class="breadcrumbs-img"
-                            style="background-image: url(https://images.unsplash.com/photo-1595341888016-a392ef81b7de?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1179&q=80);">
+                            style="background-image: url(https://images.unsplash.com/photo-1514989940723-e8e51635b782?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80">
                             <h2 class="changetowhite">Men's</h2>
                         </div>
                         <div class="menu text-center">
